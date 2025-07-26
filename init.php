b2d6a0ac6566554b11ed65b9c4104f5ca9e751aa
Roy Wenang R <119590801+roywenangr@users.noreply.github.com>
2025-07-26 18:29:41 +0700

    Delete init.php

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db = new SQLite3(__DIR__ . '/data.db');
-
-$db->exec("CREATE TABLE IF NOT EXISTS links (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    url TEXT NOT NULL,
-    active INTEGER DEFAULT 1,
-    clicks INTEGER DEFAULT 0
-)");
